d1dcc9d9b9ac01eb8714405bae450eea839607a2
eraikwar <eraikwar@unc.edu>
2024-10-10 20:59:06 -0400

    Finished EX04 (fr.)

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,17 +5,19 @@ __author__ = "730768796"
 
 def all(int_list: list[int], num: int) -> bool:
     """Checking if all the ints in the list are the same as the given int."""
-    for nums in int_list:  # goes through each value in list and checks if equal to int.
-        if nums != num:  # if not, False is returned.
-            return False
-    return True
+    for nums in int_list:  # goes through each value in list
+        if nums == num:  # checks if equal to int
+            return True
+        else:
+            return False  # if not, False is returned.
+    return False
 
 
 def max(int_list: list[int]) -> int:
     """Should return largest int in list."""
     if len(int_list) == 0:  # given. raises value error is an empty list is inputted.
         raise ValueError("max() arg is an empty List")
-    max_num: int = 0  # start with 0 because it's the lowest integer.
+    max_num: int = int_list[0]  # start with the first number in the list.
     for num in int_list:  # goes through each value in the list
         if num > max_num:  # checks if value is greater than the recorded max_num
             max_num = num  # if so, replaces max_num
@@ -24,18 +26,24 @@ def max(int_list: list[int]) -> int:
 
 def is_equal(int_list1: list[int], int_list2: list[int]) -> bool:
     """Checks if each value in the two lists are equal."""
-    index: int = 0
-    while index < len(int_list1):  # checks two lists
-        if (
-            int_list1[index] == int_list2[index]
-        ):  # both lists at the same index are checked.
-            index += 1
-        else:
-            return False
-    return True
+    if len(int_list1) == len(int_list2):
+        index: int = 0
+        while index < len(int_list1):  # checks two lists
+            if (
+                int_list1[index] == int_list2[index]
+            ):  # both lists at the same index are checked.
+                index += 1
+            else:
+                return False
+        return True
+    return False
 
 
 def extend(int_list1: list[int], int_list2: list[int]) -> None:
     """Add the second list's values to the first list."""
     for nums in int_list2:  # goes through each value in second list
         int_list1.append(nums)  # adds values from second list to first list
+
+
+def main() -> None:
+    print(all([1, 2, 2], 2))
